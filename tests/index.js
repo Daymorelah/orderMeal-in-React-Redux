1e6c0483eaf7b4a56260d0224e30548478769edb
Ademola Hussain <demola.hussainin@gmail.com>
2018-12-03 06:15:21 +0100

    [Chore 162017420] setup project environment

---
+++ tests/index.js
@@ -0,0 +1,3 @@
+
+import './setup';
+import './sampleTest.specs';
---
+++ webpack.config.dev.js
@@ -0,0 +1,41 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  devtool: 'inline-source-map',
+  mode: 'development',
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+    path.resolve(__dirname, 'src/app.js')
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    noInfo: true,
+    debug: true,
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), exclude: /node_modules/, use: "babel-loader"},
+      {test: /(\.css)$/, use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader' }
+      ]},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader'},
+      {test: /\.(woff|woff2)$/, use: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
