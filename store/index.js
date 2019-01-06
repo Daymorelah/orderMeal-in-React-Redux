9aec4bd7bc188c53f25d70a92291dde3ca42e636
Ademola Hussain <demola.hussainin@gmail.com>
2019-01-06 10:02:18 +0100

    [Chore 163006731] Setup redux to manage state for the app

---
+++ store/index.js
@@ -0,0 +1,6 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+
+const configureStore = (initialState) => createStore(rootReducer, initialState);
+
+export default configureStore;
